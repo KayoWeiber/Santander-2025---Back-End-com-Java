66687d1ae698894e651fc184bdefc23ddfabeb70
Kayo Weiber <caioveiber598@gmail.com>
2025-06-15 13:39:01 -0300

    feat(entendendoExceptions) -  creation of a user registration crud

---
+++ Sintaxe Básica Java/entendendoExceptions/debugging/src/model/MenuOption.java
@@ -0,0 +1,15 @@
+package model;
+
+import DAO.UserDAO;
+
+import java.util.function.Consumer;
+
+public enum MenuOption {
+    SAVE,
+    UPDATE,
+    DELETE,
+    FIND_BY_ID,
+    FIND_ALL,
+    EXIT;
+
+}
